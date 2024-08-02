1298dbfbdc76aca1c755a5aef7f9c756658e0667
Venom Lightning <dentatar95@gmail.com>
2024-08-02 05:02:39 +0300

    Add commands

--- src/main/java/org/nineteam/unpersistMobs/UnpersistMobs.java
+++ src/main/java/org/nineteam/unpersistMobs/UnpersistMobs.java
@@ -1,5 +1,8 @@
 package org.nineteam.unpersistMobs;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
@@ -20,13 +23,10 @@ import org.bukkit.configuration.file.FileConfiguration;
 import com.destroystokyo.paper.event.entity.EntityRemoveFromWorldEvent;
 import com.destroystokyo.paper.event.entity.EntityAddToWorldEvent;
 
-public final class UnpersistMobs extends JavaPlugin implements Listener {
+public final class UnpersistMobs extends JavaPlugin implements Listener, CommandExecutor {
     NamespacedKey unpersist = new NamespacedKey(this, "unpersist");
-    /*List<Class<?>> excludedMobs = Arrays.asList(
-                PiglinAbstract.class,
-                Raider.class
-        );*/
     FileConfiguration config = getConfig();
+    int markedCounter, despawnedCounter = 0;
 
     @Override
     public void onEnable() {
@@ -36,16 +36,38 @@ public final class UnpersistMobs extends JavaPlugin implements Listener {
         config.options().copyDefaults(true);
         saveConfig();
 
-        // Works for every monster with following exceptions:
+        // Works for every monster with the following exceptions:
         // - Endermans with block in hands are handled by Purpur
         // - Zombified Piglins from portals are disabled by Spigot
         // - Piglins/Raiders are ignored in case of specific structures
         Bukkit.getPluginManager().registerEvents(this, this);
+        getCommand("admin").setExecutor(this);
     }
 
     /*@Override
     public void onDisable() {
     }*/
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(args.length > 0) {
+            switch (args[0]) {
+                case "reload":
+                    reloadConfig();  // Discard
+                    config = getConfig();  // Update
+                    sender.sendMessage("Configuration reloaded");
+                    return true;
+                case "stats":
+                    sender.sendMessage(String.format("Mobs marked: %d (and %d already despawned)",
+                            markedCounter, despawnedCounter));
+                    return true;
+                default:
+                    return false;
+            }
+        } else {
+            return false;
+        }
+    }
 
     private void log(String verb, Mob mob) {
         Location location = mob.getLocation();
@@ -68,13 +90,14 @@ public final class UnpersistMobs extends JavaPlugin implements Listener {
             // Mobs which can pick up items and have RemoveWhenFarAway false
             //  so assume mob already picked up some items and persistent because of it
             if(mob.getCanPickupItems() && !mob.getRemoveWhenFarAway()) {
-                if(!config.getBoolean("logOnly")) {
+                if(config.getBoolean("logOnly")) {
+                    log("persisted for", mob);
+                } else {
                     // Set mob for despawn and mark for items drop
                     mob.setRemoveWhenFarAway(true);
                     mob.getPersistentDataContainer().set(unpersist, PersistentDataType.BOOLEAN, true);
+                    markedCounter++;
                     log("marked after", mob);
-                } else {
-                    log("persisted for", mob);
                 }
             }
         }
@@ -104,6 +127,7 @@ public final class UnpersistMobs extends JavaPlugin implements Listener {
                 world.dropItemNaturally(location, mainHand);
                 world.dropItemNaturally(location, offHand);
             }
+            despawnedCounter++;
             log("despawned after", mob);
         }
     }
