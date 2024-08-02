7452e6813c6e5aadf111aae232d51c63ab047429
Venom Lightning <dentatar95@gmail.com>
2024-08-02 05:56:43 +0300

    Better entities filtering

--- src/main/java/org/nineteam/unpersistMobs/UnpersistMobs.java
+++ src/main/java/org/nineteam/unpersistMobs/UnpersistMobs.java
@@ -1,15 +1,10 @@
 package org.nineteam.unpersistMobs;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.SpawnCategory;
 import org.bukkit.entity.Mob;
-import org.bukkit.entity.PiglinAbstract;
-import org.bukkit.entity.Raider;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.NamespacedKey;
@@ -19,6 +14,10 @@ import org.bukkit.Location;
 import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 
 import com.destroystokyo.paper.event.entity.EntityRemoveFromWorldEvent;
 import com.destroystokyo.paper.event.entity.EntityAddToWorldEvent;
@@ -39,7 +38,8 @@ public final class UnpersistMobs extends JavaPlugin implements Listener, Command
         // Works for every monster with the following exceptions:
         // - Endermans with block in hands are handled by Purpur
         // - Zombified Piglins from portals are disabled by Spigot
-        // - Piglins/Raiders are ignored in case of specific structures
+        // - Unnaturally spawned mobs because they generated with a structure
+        // - Custom mobs to not accidentally break other plugins behaviour
         Bukkit.getPluginManager().registerEvents(this, this);
         getCommand("admin").setExecutor(this);
     }
@@ -70,10 +70,10 @@ public final class UnpersistMobs extends JavaPlugin implements Listener, Command
     }
 
     private void log(String verb, Mob mob) {
-        Location location = mob.getLocation();
+        Location loc = mob.getLocation();
         getLogger().info(String.format("%s with uuid %s in %s at %.1f %.1f %.1f %s %.1f hours!",
                 mob.getName(), mob.getUniqueId(),
-                location.getWorld().getName(), location.x(), location.y(), location.z(),
+                loc.getWorld().getName(), loc.x(), loc.y(), loc.z(),
                 verb, (float)mob.getTicksLived() / 20 / 3600)
         );
     }
@@ -81,11 +81,12 @@ public final class UnpersistMobs extends JavaPlugin implements Listener, Command
     @EventHandler
     public void onEntityAddToWorld(EntityAddToWorldEvent event) {
         Entity entity = event.getEntity();
-        // Monsters which lived too long without name tags
+        SpawnReason reason = entity.getEntitySpawnReason();
+        // Naturally spawned monsters which lived too long without a name tag
         // NOTE: Custom name will set RemoveWhenFarAway to false, but sitting in a vehicle won't
-        if(entity.getTicksLived() > config.getInt("ticksLived") && entity.customName() == null
-                && entity.getSpawnCategory().equals(SpawnCategory.MONSTER)
-                && !(entity instanceof PiglinAbstract || entity instanceof Raider)) {
+        if(!(reason.equals(SpawnReason.DEFAULT) || reason.equals(SpawnReason.CUSTOM))
+                && entity.getSpawnCategory().equals(SpawnCategory.MONSTER) && entity.customName() == null
+                && entity.getTicksLived() > config.getInt("ticksLived")) {
             Mob mob = (Mob)entity;
             // Mobs which can pick up items and have RemoveWhenFarAway false
             //  so assume mob already picked up some items and persistent because of it
@@ -120,12 +121,12 @@ public final class UnpersistMobs extends JavaPlugin implements Listener, Command
 
                 // Drop items
                 World world = mob.getWorld();
-                Location location = mob.getLocation();
+                Location loc = mob.getLocation();
                 for (ItemStack armorPiece : armor) {
-                    world.dropItemNaturally(location, armorPiece);
+                    world.dropItemNaturally(loc, armorPiece);
                 }
-                world.dropItemNaturally(location, mainHand);
-                world.dropItemNaturally(location, offHand);
+                world.dropItemNaturally(loc, mainHand);
+                world.dropItemNaturally(loc, offHand);
             }
             despawnedCounter++;
             log("despawned after", mob);
