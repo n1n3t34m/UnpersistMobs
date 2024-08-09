804d35f86b90e877c3bbb7fe397e7a549572b952
Venom Lightning <dentatar95@gmail.com>
2024-08-09 04:03:44 +0300

    Refactor and bug fixes

--- src/main/java/org/nineteam/unpersistMobs/UnpersistMobs.java
+++ src/main/java/org/nineteam/unpersistMobs/UnpersistMobs.java
@@ -18,25 +18,33 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
+import org.bukkit.event.EventPriority;
 
 import com.destroystokyo.paper.event.entity.EntityRemoveFromWorldEvent;
 import com.destroystokyo.paper.event.entity.EntityAddToWorldEvent;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public final class UnpersistMobs extends JavaPlugin implements Listener, CommandExecutor {
     NamespacedKey unpersist = new NamespacedKey(this, "unpersist");
     FileConfiguration config = getConfig();
-    int markedCounter, despawnedCounter = 0;
+    int markedCounter, despawnedCounter, droppedCounter = 0;
 
     @Override
     public void onEnable() {
         config.addDefault("ticksLived", 1728000);
         config.addDefault("logOnly", false);
         config.addDefault("dropItems", true);
+        config.addDefault("ignoredSpawnReasons", List.of(
+                "DEFAULT", "CUSTOM", "COMMAND"));  // Only natural spawns
         config.options().copyDefaults(true);
         saveConfig();
 
         Bukkit.getPluginManager().registerEvents(this, this);
-        getCommand("admin").setExecutor(this);
+        getCommand("reload").setExecutor(this);
+        getCommand("stats").setExecutor(this);
     }
 
     /*@Override
@@ -45,83 +53,77 @@ public final class UnpersistMobs extends JavaPlugin implements Listener, Command
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(args.length > 0) {
-            switch (args[0]) {
-                case "reload":
-                    reloadConfig();  // Discard
-                    config = getConfig();  // Update
-                    sender.sendMessage("Configuration reloaded");
-                    return true;
-                case "stats":
-                    sender.sendMessage(String.format("Mobs marked: %d (and %d already despawned)",
-                            markedCounter, despawnedCounter));
-                    return true;
-                default:
-                    return false;
-            }
-        } else {
-            return false;
+        switch (command.getName()) {
+            case "reload":
+                reloadConfig();  // Discard
+                config = getConfig();  // Update
+                sender.sendMessage("Configuration reloaded");
+                return true;
+            case "stats":
+                sender.sendMessage(String.format("Mobs marked: %d\nMobs despawned: %d\nItems dropped: %d",
+                        markedCounter, despawnedCounter, droppedCounter));
+                return true;
+            default:
+                return false;
         }
     }
 
-    private void log(String verb, Mob mob) {
-        Location loc = mob.getLocation();
-        getLogger().info(String.format("%s with uuid %s in %s at %.1f %.1f %.1f %s %.1f hours!",
-                mob.getName(), mob.getUniqueId(),
+    private void log(String verb, Mob entity) {
+        Location loc = entity.getLocation();
+        getLogger().info(String.format("%s %s uuid %s in %s at %.1f %.1f %.1f %s %.1f hours!",
+                entity.getEntitySpawnReason(), entity.getType(), entity.getUniqueId(),
                 loc.getWorld().getName(), loc.x(), loc.y(), loc.z(),
-                verb, (float)mob.getTicksLived() / 20 / 3600)
+                verb, (float) entity.getTicksLived() / 20 / 3600)
         );
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onEntityAddToWorld(EntityAddToWorldEvent event) {
         Entity entity = event.getEntity();
-        SpawnReason reason = entity.getEntitySpawnReason();
-        // Naturally spawned monsters which lived too long without a name tag
-        // NOTE: Custom name will set RemoveWhenFarAway to false, but sitting in a vehicle won't
-        if(!(reason.equals(SpawnReason.DEFAULT) || reason.equals(SpawnReason.CUSTOM))
-                && entity.getSpawnCategory().equals(SpawnCategory.MONSTER) && entity.customName() == null
-                && entity.getTicksLived() > config.getInt("ticksLived")) {
-            Mob mob = (Mob)entity;
-            // Mobs which can pick up items and have RemoveWhenFarAway false
-            //  so assume mob already picked up some items and persistent because of it
-            if(mob.getCanPickupItems() && !mob.getRemoveWhenFarAway()) {
-                if(config.getBoolean("logOnly")) {
-                    log("persisted for", mob);
-                } else {
-                    // Set mob for despawn and mark for items drop
-                    mob.setRemoveWhenFarAway(true);
-                    mob.getPersistentDataContainer().set(unpersist, PersistentDataType.BOOLEAN, true);
-                    markedCounter++;
-                    log("marked after", mob);
+        if (entity.isValid() && entity.getSpawnCategory().equals(SpawnCategory.MONSTER)) {  // Only monsters
+            SpawnReason reason = entity.getEntitySpawnReason();
+            if (!config.getStringList("ignoredSpawnReasons").contains(reason.toString())) {  // Filter reason
+                Mob mob = (Mob) entity;
+                if (!mob.getRemoveWhenFarAway() && mob.customName() == null  // Persistent without nametag
+                        && mob.getTicksLived() > config.getInt("ticksLived")) {  // Lived for too long
+                    if (config.getBoolean("logOnly")) {
+                        log("persisted for", mob);
+                    } else {
+                        // Set mob for despawn and mark
+                        mob.setRemoveWhenFarAway(true);
+                        mob.getPersistentDataContainer().set(unpersist, PersistentDataType.BOOLEAN, true);
+                        markedCounter++;
+                        log("marked after", mob);
+                    }
                 }
             }
         }
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onEntityRemoveFromWorld(EntityRemoveFromWorldEvent event) {
         Entity entity = event.getEntity();
-        // Detect despawn of marked mob
-        // FIXME: Must be replaced with event cause in the future
-        if(entity.getPersistentDataContainer().getOrDefault(unpersist, PersistentDataType.BOOLEAN, false)
-                && entity.isDead() && entity.getChunk().isLoaded()) {
-            Mob mob = (Mob)entity;
-            if(config.getBoolean("dropItems")) {
+        if (entity.isDead() && entity.getChunk().isLoaded()  // FIXME: Replace with event cause in the future
+                && entity.getPersistentDataContainer().getOrDefault(unpersist, PersistentDataType.BOOLEAN, false)) {
+            Mob mob = (Mob) entity;
+            if (mob.getCanPickupItems() && config.getBoolean("dropItems")) {  // Entity may picked up something
+                List<ItemStack> items = new ArrayList<>();
+
                 // Get items
                 EntityEquipment equipment = mob.getEquipment();
-                ItemStack[] armor = equipment.getArmorContents();
-                ItemStack mainHand = equipment.getItemInMainHand();
-                ItemStack offHand = equipment.getItemInOffHand();
+                items.add(equipment.getItemInMainHand());
+                items.add(equipment.getItemInOffHand());
+                items.addAll(Arrays.asList(equipment.getArmorContents()));
 
                 // Drop items
                 World world = mob.getWorld();
                 Location loc = mob.getLocation();
-                for (ItemStack armorPiece : armor) {
-                    world.dropItemNaturally(loc, armorPiece);
+                for (ItemStack item : items) {
+                    if (!item.getType().isEmpty()) {
+                        droppedCounter++;
+                        world.dropItemNaturally(loc, item);
+                    }
                 }
-                world.dropItemNaturally(loc, mainHand);
-                world.dropItemNaturally(loc, offHand);
             }
             despawnedCounter++;
             log("despawned after", mob);
